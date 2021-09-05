e6e3493adcc15238ae4c68c80e1f82d2716f8d4d
palash sarkar <spalash521@gmail.com>
2021-09-05 22:33:51 +0300

    first commit

---
+++ time.js
@@ -0,0 +1,14 @@
+function dosomesThing() {
+    console.log('i am conding javascript')
+}
+console.log('i am first');
+console.log('i am second');
+// setTimeout(dosomesThing, 3500);
+setTimeout(function () {
+    console.log('i am waiting for you')
+}, 5000)
+setTimeout(() => {
+    console.log('exploring the article')
+}, 4000)
+console.log('i am thrist');
+console.log('i am four');
