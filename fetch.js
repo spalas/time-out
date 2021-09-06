c70a2ffd7fd743ba0d9e29e1999510bc321d0896
palash sarkar <spalash521@gmail.com>
2021-09-06 22:18:50 +0300

    add all to file

--- fetch.js
+++ fetch.js
@@ -1,18 +1,45 @@
-console.log('1111');
-console.log('2222');
-// setTimeout(() => console.log('aaaa'), 5000);
-// fetch as an asynchronous this one work at last 
-// same as settimeout work ai ta sobar pare kaj kore so kaj shese kaj kore 
-// ar jonnay ai ta ke asynchronus bole
+// console.log('1111');
+// console.log('2222');
+// // setTimeout(() => console.log('aaaa'), 5000);
+// // fetch as an asynchronous this one work at last 
+// // same as settimeout work ai ta sobar pare kaj kore so kaj shese kaj kore 
+// // ar jonnay ai ta ke asynchronus bole
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
 
-console.log('3333');
-console.log('4444');
-console.log('5555');
-for (let i = 0; i < 1000; i++) {
-    console.log(i);
+// console.log('3333');
+// console.log('4444');
+// console.log('5555');
+// for (let i = 0; i < 1000; i++) {
+//     console.log(i);
 
+// }
+// alert how use 
+console.log('i am coming ')
+const motherComing = () => {
+    alert('i am coming mama open the door dear jsut wait')
+};
+
+
+// confirm use how 
+const askingPicnic = () => {
+    const response = confirm('Are you going to picnic?');
+    console.log(response);
+    if (response === true) {
+        alert('send bikash')
+    }
+    else {
+        console.log('go to hell')
+    }
+};
+
+// prompt how to use
+
+const askName = () => {
+    const name = prompt('what is you name?');
+    if (name) {
+        console.log(name)
+    }
 }
--- setinterval.js
+++ setinterval.js
@@ -1,17 +1,28 @@
-console.log('first');
-// setInterval(() => {
-//     console.log('third')
+// console.log('first');
+// // setInterval(() => {
+// //     console.log('third')
 
-// }, 3000);
-let seconds = 0;
-const timeId = setInterval(() => {
-    // seconds++;
-    seconds++;
-    console.log(seconds);
-    if (seconds > 20) {
-        clearInterval(timeId)
-    }
+// // }, 3000);
+// let seconds = 0;
+// const timeId = setInterval(() => {
+//     // seconds++;
+//     seconds++;
+//     console.log(seconds);
+//     if (seconds > 20) {
+//         clearInterval(timeId)
+//     }
 
-}, 1000);
+// }, 1000);
 
-console.log('second')
+// // console.log('second');
+
+
+// console.log(a);
+// var a = 10
+// // console.log(a)
+
+// const v = 10
+// function hello() {
+//     console.log(v)
+// }
+// hello()
